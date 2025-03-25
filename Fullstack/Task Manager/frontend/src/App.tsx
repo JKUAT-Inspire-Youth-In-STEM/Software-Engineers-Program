6be65200fa0cabdaf7c86925d5213b36e3e19bdc
Emmanuel-Odero <emmanuelodero808@gmail.com>
2025-03-25 11:18:08 +0300

    frontend code

--- Fullstack/Task Manager/frontend/src/App.tsx
+++ Fullstack/Task Manager/frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { Landingpage } from "./components/Landingpage";
+import { SignUp } from "./components/Signup";
+import { SignIn } from "./components/SignIn";
 
 function App() {
 
   return (
-    <>
-      <h1 className="underline text-red-800">Welcome to task manager</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landingpage />}/>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
   )
 }
 
