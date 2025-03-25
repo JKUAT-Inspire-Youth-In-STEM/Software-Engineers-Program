8bdb39cb21a8e7f200fd743969461b66dddf8b04
Emmanuel-Odero <emmanuelodero808@gmail.com>
2025-03-25 16:40:48 +0300

    front end ready

---
+++ Fullstack/Task Manager/frontend/src/types.ts
@@ -0,0 +1,26 @@
+export interface User {
+  username: string;
+  password: string;
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  start_time: string;
+  duration: number;
+  done: boolean;
+}
+
+export interface TaskCreatePayload {
+  title: string;
+  description?: string;
+  duration: number;
+  start_now?: boolean; // Matches backend expectation
+  done?: boolean;
+}
+
+export interface AuthResponse {
+  msg?: string;
+  token?: string;
+}
